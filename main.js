86747d71733234403df46ab4344e5105513fba5c
ちと <chitose.dev@outlook.jp>
2024-10-24 23:17:32 +0900

    updata

--- main.js
+++ main.js
@@ -19,3 +19,33 @@ const markdownToHtml = markdown => {
     .replace(/^(?!<h\d|<a|<\/p>)(.+)$/gm, (_, text) => {return `<p>${text}</p>`;});
     return html;
 };
+
+const buildArticle = async (filePath) => {
+    const content = await fs.promises.readFile(filePath, "utf-8");
+    const fileName = path.basename(filePath, ".md") + ".html";
+    const outputPath = path.join("/article", fileName);
+    const htmlContent = markdownToHtml(content);
+
+    const html = `
+    <html>
+      <head>
+       <title></title>
+      </head>
+      <body>
+        <header></header>
+        <main>
+          <h1 id="title"></h1>
+          <div id="content"></div>
+        </main>
+        <footer></footer>
+      </body>
+    </html>`;
+    await fs.promises.writeFile(outputPath, html, "utf-8");
+    console.log("built article!")
+};
+
+const articles = await fs.promises.readdir("/article/articles");
+const tasks = articles
+.filter(file => file.endsWith(".md"))
+.map(file => buildArticle(path.join("/article/articles", file)));
+buildArticle(tasks);
