c9644d9f9e1caf808f9d87c2573af00bad38dd82
ちと <chitose.dev@outlook.jp>
2024-10-24 22:55:42 +0900

    updata

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
 
+const markdownToHtml = markdown => {
+    let id = 0;
+    const toc = [];
+    const html = markdown
+    .replace(/^# (.*$)/gm, (match, p1) => {
+        toc.push({ id: `heading-${id + 1}`, text: p1, el: "h2" });
+        return `<h2 id="${++id}">${p1}</h2>`;
+    })
+    .replace(/^## (.*$)/gm, (match, p1) => {
+        toc.push({ id: `heading-${id + 1}`, text: p1, el: "h3" });
+        return `<h3 id="${++id}">${p1}</h3>`;
+    })
+    .replace(/\[(.*?)\]\((.*?)\)/g, (_, text, url) => {
+       return `<a href="${url}">${text}</a>`;
+    })
+    .replace(/^(?!<h\d|<a|<\/p>)(.+)$/gm, (_, text) => {return `<p>${text}</p>`;});
+    return html;
+};
