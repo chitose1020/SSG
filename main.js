8de60db2d32192ff2dfa3f2adec8c1dc36c211dd
ちと <chitose.dev@outlook.jp>
2024-10-25 22:54:21 +0900

    updata

--- main.js
+++ main.js
@@ -4,24 +4,8 @@ const path = require("path");
 const markdownToHtml = (markdown) => {
   let id = 0;
   const toc = [];
-  const html = markdown
-    .replace(/^# (.*$)/gm, (match, p1) => {
-      toc.push({ id: `heading-${id + 1}`, text: p1, el: "h2" });
-      return `<h2 id="${++id}">${p1}</h2>`;
-    })
-    .replace(/^## (.*$)/gm, (match, p1) => {
-      toc.push({ id: `heading-${id + 1}`, text: p1, el: "h3" });
-      return `<h3 id="${++id}">${p1}</h3>`;
-    })
-    .replace(/\[(.*?)\]\((.*?)\)/g, (_, text, url) => {
-      return `<a href="${url}">${text}</a>`;
-    })
-    .replace(/ {2,}\n/g, '<br>\n')
-    .replace(/\n{2,}/g, '</p><p>')
-    .replace(/^(?!<h\d|<a|<\/p>)(.+)$/gm, (_, text) => {
-      return `<p>${text}</p>`;
-    });
-  return html;
+  const paragraphs = markdown.split(/\n{2,}/);
+  console.log(paragraphs);
 };
 
 const buildArticle = async (filePath) => {
