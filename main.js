3f8715f79e3e6d37b5263be1180666ca60307a47
ちと <chitose.dev@outlook.jp>
2024-10-25 16:16:46 +0900

    updata

--- main.js
+++ main.js
@@ -31,7 +31,13 @@ const buildArticle = async (filePath) => {
   const html = `
     <html>
       <head>
-       <title></title>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="description" content="">
+          <link rel="shortcut icon" href="/public/img/png/favicon.png" type="image/x-icon">
+          <link rel="stylesheet" href="/public/css/common.css>
+          <script src="/public/js/common.js" defer></script>
+          <title></title>
       </head>
       <body>
         <header></header>
